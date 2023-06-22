fc6d5411ecd3d4f3803a82e0c4af9e1ce985887a
Govind Kumar <62988424+govind527@users.noreply.github.com>
2023-06-22 11:44:55 +0530

    Update 50 day challenge.sql

--- 50 day challenge.sql
+++ 50 day challenge.sql
@@ -1,6 +1,6 @@
 Challenge1:
 
 Mr. Ram wants to go for a movie along with his family. He wants to book three consecutive seats. 
-Can you help him get the list of 3 or more consecutive empty seats?
+Can you help him get the list of 3 or more consecutive empty seats?.
  
 
